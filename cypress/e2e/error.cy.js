e1afaed73bd22f3981785fa2311b7ee33ae579c0
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-11-15 11:30:12 -0700

    error 1

--- cypress/e2e/error.cy.js
+++ cypress/e2e/error.cy.js
@@ -1,5 +1,10 @@
 describe('iteration 5, Error Handling Flows', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
+ it("Should show error when all urls cannot be retrieved", () => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/urls", {
+      forceNetworkError: true,
+    }).as("error");
+    cy.visit("http://localhost:3000/");
+    cy.get("p").contains("No urls yet! Find some to shorten!");
+})
+
 })
