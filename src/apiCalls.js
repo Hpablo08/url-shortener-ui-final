7a84302505a3c6617ac8b2be70e2cda5f5c08b7c
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-11-15 11:08:25 -0700

    fix fetch and add post call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,26 @@
-export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
-}
+export const getUrls = async () => {
+  return await fetch("http://localhost:3001/api/v1/urls");
+};
+
+export const postUrls = (newUrl) => {
+  const requestData = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      long_url: newUrl.urlToShorten,
+      title: newUrl.title,
+    }),
+  };
+  console.log(requestData);
+  return fetch("http://localhost:3001/api/v1/urls", requestData)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Not a 200 status");
+      }
+      alert("Information submitted");
+      return response.json();
+    })
+    .catch((error) => {
+      alert("Oops, something went wrong in the post. Try again later");
+    });
+};
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,27 +3,52 @@ import './App.css';
 import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
+import { postUrls } from '../../apiCalls';
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: []
-    }
+      urls: [],
+    };
   }
 
-  componentDidMount() {
-  }
+  componentDidMount = async () => {
+    this.fetchUrl();
+  };
+  fetchUrl = async () => {
+    try {
+      const urlList = await getUrls();
+      const data = await urlList.json();
+      console.log(data);
+      this.setState({ urls: data.urls });
+    } catch {
+      this.setState({
+        error: "error getting urls",
+      });
+      console.log(this.state.error);
+    }
+  };
+  addNewUrl = (newUrl) => {
+    console.log(newUrl);
+    postUrls(newUrl)
+      .then((data) => this.setState({ urls: [...this.state.urls, data] }))
+      .catch((error) => {
+        this.setState({
+          error: `Oops! That's a ${error.message}. Something went wrong adding your URL. Make sure you filled in all input fields!`,
+        });
+      });
+  };
 
   render() {
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addNewUrl={this.addNewUrl} />
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={this.state.urls} />
       </main>
     );
   }
